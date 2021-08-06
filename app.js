331801d9119d56378fac1e48892de7dd8d08e41a
DevZeppelin <gino.b.pietrobon@gmail.com>
2021-08-06 10:34:56 -0300

    change method to listen changes in checkbox of dark mode

--- app.js
+++ app.js
@@ -1,4 +1,31 @@
+/*
+Funciona agregando onchange en el check box con switchTheme()
 
-function switchTheme (){    
+<input type.. clas.. id.. onchange="switchTheme()"
+
+function switchTheme (){
     document.body.classList.toggle("dark-mode")
 }
+ */
+
+const checkbox = document.querySelector('#check-box')
+
+console.log(checkbox)
+
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    checkbox.setAttribute('checked', true)
+}
+
+checkbox.addEventListener('change', function (event) {
+    
+    event.preventDefault()
+
+    if (this.checked) {
+        document.body.classList.remove('is-light-mode')
+        document.body.classList.add('is-dark-mode')
+    } else {
+        document.body.classList.remove('is-dark-mode')
+        document.body.classList.add('is-light-mode')
+    }
+})
+
